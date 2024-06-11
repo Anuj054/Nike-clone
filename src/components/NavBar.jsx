2e018171a526e50ee895302859c5728db7812e46
Anuj054 <anujc054@gmail.com>
2024-06-12 00:39:43 +0530

    navbar done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -26,17 +26,17 @@ const NavBar = () => {
   return (
     <>
       <header className={!navState?  "absolute top-7 left-0 right-0 opacity-100 z-50":
-       " fixed top-0 left-0 right-0 h-[9vh]  flex items-center justify-center opacity-100 z-[100] blur-effect-theme  "}>
+       " fixed top-0 left-0 right-0 h-[9vh]  flex items-center justify-center opacity-100 z-[200] blur-effect-theme  "}>
         <nav className=" flex items-center justify-between nike-container">
           <div className=" flex items-center">
-            <img src={logo} alt="logo/img" className=" h-auto w-16 " />
+            <img src={logo} alt="logo/img" className={` h-auto w-16 ${navState && "filter brightness-0"}`} />
           </div>
           <ul className=" flex items-center justify-center gap-2">
             <li className=" grid items-center">
-              <MagnifyingGlassIcon className="icon-style" />
+              <MagnifyingGlassIcon className={`icon-style ${navState && " text-slate-900 transition-all duration-300"}`} />
             </li>
             <li className=" grid items-center">
-              <HeartIcon className="icon-style" />
+              <HeartIcon className={`icon-style ${navState && " text-slate-900 transition-all duration-300"}`} />
             </li>
             <li className=" grid items-center">
               <button
@@ -44,13 +44,14 @@ const NavBar = () => {
                 className=" border-none outline-none active:scale-110 
                 transition-all duration-300 relative"
               >
-                <ShoppingBagIcon className="icon-style" />
+                <ShoppingBagIcon className={`icon-style ${navState && " text-slate-900 transition-all duration-300"}`} />
                 <div
-                  className={` absolute top-4 right-0 bg-white
-                   text-slate-900 shadow shadow-slate-100 w-4 h-4 
+                  className={` absolute top-4 right-0 w-4 h-4 
                    text-[0.65rem]  leading-tight font-medium rounded-full flex
                  items-center justify-center cursor-pointer hover:scale-110 
-                 transition-all duration-300 `}
+                 transition-all duration-300  ${navState ? 
+                  ' bg-slate-900 text-slate-100 shadow-slate-900':
+                  ' bg-slate-100 text-slate-900 shadow-slate-100'}`}
                 >
                   0
                 </div>
