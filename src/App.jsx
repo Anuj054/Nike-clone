720d53549310c1ac32aab2f3cf4e22b8a5cce23f
Anuj054 <anujc054@gmail.com>
2024-06-12 00:15:42 +0530

    Navbar and footer done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
-import React from 'react';
-import { FlexContent, Hero ,Sales, Stories} from './components';
-import {heroapi , popularsales,toprateslaes,highlight,sneaker,story} from './data/data';
+import React from "react";
+import { FlexContent, Footer, Hero, NavBar, Sales, Stories } from "./components";
+import {
+  heroapi,
+  popularsales,
+  toprateslaes,
+  highlight,
+  sneaker,
+  story,
+  footerAPI,
+} from "./data/data";
 const App = () => {
   return (
     <div>
-     <main className=' flex flex-col relative gap-16'>
-      <Hero heroapi={heroapi}/>
-      <Sales  endpoint ={popularsales} ifExists />
-      <FlexContent endpoint={highlight} ifExists/>
-      <Sales endpoint= {toprateslaes} />
-      <FlexContent endpoint={sneaker}/> 
-      <Stories story={story}/>
-     </main>
+      <NavBar/>
+      <main className=" flex flex-col relative gap-16">
+        <Hero heroapi={heroapi} />
+        <Sales endpoint={popularsales} ifExists />
+        <FlexContent endpoint={highlight} ifExists />
+        <Sales endpoint={toprateslaes} />
+        <FlexContent endpoint={sneaker} />
+        <Stories story={story} />
+      </main>
+      <Footer footerAPI ={footerAPI}/>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
